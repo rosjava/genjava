788c1f59102aac937f0ca48515b3937bcbe4c21d
Daniel Stonier <d.stonier@gmail.com>
2014-06-25 19:14:54 +0900

    clean out comments.

--- src/genjava/genjava_main.py
+++ src/genjava/genjava_main.py
@@ -73,41 +73,4 @@ def parse_arguments(argv):
 def main(argv):
     args = parse_arguments(argv[1:])
     #print("genjava %s/%s" % (args.package, args.message))
-    print("  output dir..........%s" % args.output_dir)
     gradle_project.create(args.package, args.output_dir)
-    #search_path = genmsg.command_line.includepath_to_dict(args.include_path)
-    #print("  search path.......%s" % search_path)
-    #gradle_project_dir = os.path.join(args.output_dir, 'gradle')
-    #os.mkdir(gradle_project_dir)
-
-#     try:
-#         if options.initpy:
-#             if options.outdir:
-#                 retcode = write_modules(options.outdir)
-#             else:
-#                 parser.error("Missing args")
-#         else:
-#             if len(args) < 2:
-#                 parser.error("please specify args")
-#             if not os.path.exists(options.outdir):
-#                 # This script can be run multiple times in parallel. We
-#                 # don't mind if the makedirs call fails because somebody
-#                 # else snuck in and created the directory before us.
-#                 try:
-#                     os.makedirs(options.outdir)
-#                 except OSError as e:
-#                     if not os.path.exists(options.outdir):
-#                         raise
-#             search_path = genmsg.command_line.includepath_to_dict(options.includepath)
-#             retcode = gen.generate_messages(options.package, args[1:], options.outdir, search_path)
-#     except genmsg.InvalidMsgSpec as e:
-#         print("ERROR: ", e, file=sys.stderr)
-#         retcode = 1
-#     except MsgGenerationException as e:
-#         print("ERROR: ", e, file=sys.stderr)
-#         retcode = 2
-#     except Exception as e:
-#         traceback.print_exc()
-#         print("ERROR: ",e)
-#         retcode = 3
-#     sys.exit(retcode or 0)
